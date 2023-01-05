8b4cdfba206a3060325c7b0499394ccdffab3642
aman9572 <amansinghrajput9572@gmail.com>
2023-01-06 03:29:31 +0530

    first commit

---
+++ client/src/services/helper.js
@@ -0,0 +1,14 @@
+// export const BASE_URL = "http://localhost:6010"
+// export const BASE_URL = "https://mernappbackend.onrender.com"
+export const BASE_URL = "https://admin-88uk.onrender.com"
+
+
+
+
+
+// "build": "react-scripts build",
+// "start": "react-scripts start",
+   
+// "test": "react-scripts test",
+// "eject": "react-scripts eject",
+// "build": "tsc"
